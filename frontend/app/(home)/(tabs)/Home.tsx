939431a9bf478fb613df66eaab700d6c9beb630e
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-03-22 20:50:26 +0530

    fix: navigation error fix

---
+++ frontend/app/(home)/(tabs)/Home.tsx
@@ -0,0 +1,32 @@
+import { View, StyleSheet, Button } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
+export default function Home() {
+
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+});
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,16 +1,31 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import { PaperProvider, Text } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import AllRoutes from '../AllRoutes';
-import Login from './Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { useNavigation } from 'expo-router';
+import { Redirect } from 'expo-router';
+import { Link } from 'expo-router';
+import AllRoutes from './screens/_layout';
+import Login from './screens/Login';
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation();
+
+  const Stack = createNativeStackNavigator();
+
+  function onPressHandler() {
+    return (
+      <Redirect href="/(home)/(tabs)/Home"/>
+    )
+  }
 
   return (
     <PaperProvider>
       <SafeAreaProvider>
-        <Login />
+        <Text>Welcome</Text>
+        <Button title='Press me' onPress={() => {onPressHandler()}}/>
       </SafeAreaProvider>
     </PaperProvider>
   );
